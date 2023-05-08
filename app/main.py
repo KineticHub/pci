1912bfba8102bf96e5e1bae4833b12bdfd2152b1
K Alnajar <1275670+KineticHub@users.noreply.github.com>
2023-05-09 03:59:50 +0800

    initial commit

---
+++ app/main.py
@@ -0,0 +1,34 @@
+import os
+
+import psycopg2 as psycopg2
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/database/version")
+async def check_database_connection():
+    conn = psycopg2.connect(
+        host=os.environ.get('DB_HOST', "localhost"),
+        database=os.environ.get('DB_NAME'),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASSWORD'),
+        port=os.environ.get('DB_PORT', "5432")
+    )
+
+    cur = conn.cursor()
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    return {"message": f"Version: {db_version}"}
+
